5b82b3d6d5b8abd3186a731f20a654c5651d1a5a
wangyue <sean@portkey.finance>
2023-12-13 10:22:15 +0800

    feat:add comment

--- contract/Portkey.Contracts.CryptoBox/CryptoBoxContractState.cs
+++ contract/Portkey.Contracts.CryptoBox/CryptoBoxContractState.cs
@@ -8,17 +8,36 @@ namespace Portkey.Contracts.CryptoBox
     /// </summary>
     public partial class CryptoBoxContractState : ContractState
     {
-        // state definitions go here.
-        public MappedState<string, CryptoBoxInfo> CryptoBoxInfoMap { get; set; }
-
+        /// <summary>
+        /// CryptoBoxInfoId -> CryptoBoxInfo
+        /// </summary>
+        public MappedState<string,CryptoBoxInfo> CryptoBoxInfoMap { get; set; }
+        
+        /// <summary>
+        /// Contracts Admin
+        /// </summary>
         public SingletonState<Address> Admin { get; set; }
-
+        
+        /// <summary>
+        /// Is contracts initialized
+        /// </summary>
         public SingletonState<bool> Initialized { get; set; }
-
-        public MappedState<string, AddressList> AlreadySnatchedList { get; set; }
-
-        public SingletonState<long> CryptoBoxMaxCount { get; set; }
-
+        
+        /// <summary>
+        /// CryptoBoxInfoId -> AddressList of already snatched
+        /// </summary>
+        public MappedState<string,AddressList> AlreadySnatchedList{ get; set; }
+        
+        /// <summary>
+        /// CryptoBoxMaxCount
+        /// </summary>
+        public SingletonState<long> CryptoBoxMaxCount{ get; set; }
+        
+        /// <summary>
+        /// TransferControllers
+        /// </summary>
         public SingletonState<ControllerList> TransferControllers { get; set; }
+
+
     }
 }
